0e1ebdcdef8614c687e7bb3a16c39fa1d3555ba0
lakshmanlearning <lakshmanlearning@gmail.com>
2021-04-27 00:54:31 +0530

    Generic Unit Of Work Implemented

---
+++ DotNet-WebAPI-Angular/DotNet-WebAPI-Angular-APIServices/Generic/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DotNet_WebAPI_Angular_Data.Models;
+using DotNet_WebAPI_Angular_InterfaceContracts.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_WebAPI_Angular_APIServices.Generic
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public MyOnlineShoppingContext Context { get; }
+
+        public UnitOfWork(MyOnlineShoppingContext context)
+        {
+            Context = context;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
---
+++ DotNet-WebAPI-Angular/DotNet-WebAPI-Angular-InterfaceContracts/Generic/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DotNet_WebAPI_Angular_Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_WebAPI_Angular_InterfaceContracts.Generic
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        MyOnlineShoppingContext Context { get; }
+
+        void Commit();
+
+        Task CommitAsync();
+    }
+}
